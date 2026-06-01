96b7af1324195dcc4ad77d2f16eacffe958caff2
Arjay Laurenz Aquino <aquinoarjay141@gmail.com>
2026-06-01 15:21:35 +0200

    second

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,32 @@
-const dati: unknown = undefined
+
+//snack 1 + bonus
+
+let dati: unknown
 
 if (typeof dati === 'string') {
   const datiInMaiuscolo = dati.toUpperCase()
   console.log(datiInMaiuscolo)
-} if (typeof dati === 'number') {
+} else if (typeof dati === 'number') {
   const datoPerDue = dati * 2
   console.log(datoPerDue)
-} if (typeof dati === 'boolean') {
+} else if (typeof dati === 'boolean') {
   console.log(dati ? 'Sì' : 'No')
+} else if (dati === null) {
+  console.log('Il dato è vuoto')
+} else if (Array.isArray(dati)) {
+  console.log(dati.length)
+} else if (dati instanceof Promise) {
+  dati.then((msg) => console.log(msg))
 } else {
   console.log('Dato non supportato')
+}
+
+// snack 2
+
+type Dipendente = {
+  nome: 'string',
+  cognome: 'string',
+  annoNascita: 'number',
+  sesso: 'm' | 'f',
+  anniDiServizio: [number]
 }
